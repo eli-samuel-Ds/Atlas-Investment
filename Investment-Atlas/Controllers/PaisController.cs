fb890764a271ea4d36426542bb46c117f9d791fc
eli-samuel-Ds <elisamuelsuerorodriguez@gmail.com>
2025-05-25 00:28:49 -0400

    Configuracion inicial Vista y algunos cambios

---
+++ Investment-Atlas/Controllers/PaisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Investment_Atlas.Controllers
+{
+    public class PaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Investment-Atlas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Investment_Atlas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
